c8bbcab5105c6274ce9cddfc218398d947bf0d56
windsywinds <windsor.sam@gmail.com>
2023-03-15 22:09:02 +1300

    intial

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -5,7 +5,7 @@ function Intro() {
         <div clasName="flex items-center justify-center flex-col text-center pt-20 pb-6">
             <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold"> Sam Windsor</h1>
             <p className="text-base md:text-xl mb-3 font-medium"> Web Developer</p>
-            <p className="text-sm mb-6 font-bold"> This is my bio here, lorem ipsum. This is my bio here, lorem ipsum. This is my bio here, lorem ipsum. This is my bio here, lorem ipsum. This is my bio here, lorem ipsum. This is my bio here, lorem ipsum. This is my bio here, lorem ipsum. </p>
+            <p className="text-sm mb-6 font-bold"> This is my bio here, lorem ipsum. This is my bio here, lorem ipsum. This is my bio here, lorem ipsum. This is my bio here, lorem ipsum. This is my bio here, lorem ipsum. This is my bio here, lorem ipsum. . </p>
         </div>
     )
 }
