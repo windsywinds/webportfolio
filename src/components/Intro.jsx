cf872782082f15f65e1fa902a39ac3235d0bbbed
windsywinds <windsor.sam@gmail.com>
2023-04-04 14:27:23 +1200

    intro update

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -9,8 +9,10 @@ function Intro() {
 
             <div className="flex items-center justify-center flex-col text-center pt-20 pb-6" >
            
-            <p className="text-sm mb-6 font-bold"> I've recently completed a Diploma in Web Development and Design and I'm eager to get started in the industry putting what I've learned to use, and continuing to grow as a developer. I built this site using react.js, however I also have experience with other languages such as Python, C#, and MySQL, which I got the chance to experience during my studies.</p>
-            <p className="text-sm mb-6 font-bold">I'm also a photography enthusiast, and SBINZ level 3 snowboard coach, having worked all over the world. In my spare time I (attempt to) surf, or get out for hikes so I can take more photos! </p>
+            <p className="text-sm mb-2 font-bold">Hi! I'm Sam, a Web Developer, Photographer, and Snowboard coach. 
+            After hanging up my snowboard boots for a career change, I completed a Diploma in Web Development and Design to kickstart my learning. I primarily work with front end languages like Javascript and HTML, however I do have some some experience with other languages such as Python, C#, and MySQL, which I got the chance to experience during my studies.</p>
+            <p className="text-sm mb-2 font-bold">In my spare time I love to get out for hikes to take photos, (attempt to) surf, or work on my own personal web projects.</p>
+            <p className="text-sm mb-6 font-bold">I've included some notable projects I've worked on below and included a contact form to get in touch. I'd love to work with you and your team!</p>
             </div>
         </div>
     )
